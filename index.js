9904ca761ea150812e349f9278a591767ae6889c
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-07-23 22:15:43 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("Rafi Khan");
 
+var base = prompt("Enter Base=");
+var height = prompt("Enter Height=");
+
+var area = base * height;
+
+// var area = (base * height) /2 ; this is how we find Rae of Triangle
+document.write("Area="+ area);
