e3841dc24dde78520c5640aabd082d61bbeeae44
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-07-25 16:21:02 +0600

    Add files via upload

---
+++ Switch/index.js
@@ -0,0 +1,67 @@
+// switch
+
+var digit=prompt("Enter Any Digit:");
+
+switch(digit){
+    case "0":
+        document.write("Zero");
+        break;
+    case "1":
+        document.write("One");
+        break;
+    case "2":
+        document.write("two");
+        break;
+    case "3":
+        document.write("Three");
+        break;
+    case "4":
+        document.write("Four");
+        break;
+    case "5":
+        document.write("Five");
+        break;
+    case "6":
+        document.write("Six");
+        break;
+    case "7":
+        document.write("Seven");
+        break;
+    case "8":
+        document.write("Eight");
+        break;
+    case "9":
+        document.write("Nine");
+        break;
+    default: //just like else
+        document.write("Not a Valid Digit");
+}
+
+
+
+//IF,Else Statement Use Digit Spelling
+
+/*var digit=prompt("Enter Any Digit: ");
+
+if(digit==0)
+    document.write("Zero");
+else if(digit==1)
+    document.write("One");
+else if(digit==2)
+    document.write("Two");
+else if(digit==3)
+    document.write("Three");
+else if(digit==4)
+    document.write("Four");
+else if(digit==5)
+    document.write("Five");
+else if(digit==6)
+    document.write("Six");
+else if(digit==7)
+    document.write("Seven");
+else if(digit==8)
+    document.write("Eight");
+else if(digit==9)
+    document.write("Nine");
+else
+    document.write("Not a valid Digit");*/
