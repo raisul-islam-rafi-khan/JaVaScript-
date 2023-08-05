5c4826fd9ea84f29e86ec105cb411c4bc487ea72
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-05 20:25:21 +0600

    Add files via upload

---
+++ Date object and Date Method/DATE_OBJECT.js
@@ -0,0 +1,23 @@
+var date=new Date();
+console.log(date);
+
+var year= date.getFullYear();
+console.log(year);
+
+var month=date.getMonth();
+console.log(month);
+
+var date=date.getDate();
+console.log(date);
+
+/*
+var currentday=date.getDay();
+console.log(currentday);
+
+var currenthour=date.getHours();
+console.log(currenthour);
+
+var CurrentMintues = date.getMinutes();
+console.log(CurrentMintues);
+
+*/
