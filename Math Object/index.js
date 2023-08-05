0aa8119c6b047dbf7841e9bf2cca4516bd26d371
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-05 14:56:15 +0600

    Add files via upload

---
+++ Math Object/index.js
@@ -0,0 +1,40 @@
+var num1=parseInt(prompt("Enter Your First number: "));
+var num2=parseInt(prompt("Enter Your second number: "));
+
+var maximum = Math.max(num1,num2);
+console.log(maximum);
+
+/*
+
+var sqaureRoot= Math.sqrt(num1,num2);
+console.log(sqaureRoot);
+
+var Sin= Math.sin(num1,num2);
+console.log(Sin);
+
+var Cos= Math.cos(num1,num2);
+console.log(Cos);
+
+var Floor= Math.floor(num1,num2);
+console.log(Floor);
+
+var Ciel= Math.ceil(num1,num2);
+console.log(Ciel);
+
+
+*/
+
+//Random number generate (0-1)
+//Math.random();
+
+
+//Random number generate (0-5)
+//Math.random()*6;
+
+//Integer Value Generate
+//Math.floor (Math.random()*6);
+
+//(1-5) random number generate
+//Math.floor (Math.random()*5)+1;
+
+
