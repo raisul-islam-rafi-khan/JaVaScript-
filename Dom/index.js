bfc6ad1a2dd5cf75e410e1a305b8dc29afaacb3c
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-05 22:10:02 +0600

    Add files via upload

---
+++ Dom/index.js
@@ -0,0 +1,20 @@
+// document.getElementById("page").innerHTML="Hello";
+
+var mypage =document.getElementById("page")
+mypage.innerHTML="Hello";
+
+
+document.getElementById("para").innerHTML="Bye bye";
+
+var myhead =document.getElementById("head")
+myhead.innerHTML="good bye";
+
+
+//document.getElementsByTagName("para")[0].innerHTML="Bye bye";
+//document.getElementsByClassName("para")[1].innerHTML="Bye bye";
+
+
+
+
+
+
