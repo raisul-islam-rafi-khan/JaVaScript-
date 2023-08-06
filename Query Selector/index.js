06307eb01d44625fecf4ae3894ab9d14aa486886
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-06 13:52:24 +0600

    Add files via upload

---
+++ Query Selector/index.js
@@ -0,0 +1,18 @@
+//getElementsById
+//getElementsByTagName
+//getElementsByClassName
+//querySelector
+
+//document.querySelector("#head").innerHTML="My Name Is Khan "; //getElementsById
+
+//document.querySelector(".head2").innerHTML=" Iam From Bangladesh "; //getElementsByClassName
+
+//document.querySelector("p").innerHTML=" Hi "; //getElementsByTagName
+
+document.querySelector("li a").innerHTML=" Office ";
+document.querySelector(".my-div a").innerHTML=" Hi ";
+
+
+
+
+
