9a71a85635296b7c3a6eb1a944f2373cdfe72232
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-06 21:17:08 +0600

    Add files via upload

---
+++ Event Handler to OneClick Event/index.js
@@ -0,0 +1,39 @@
+//alert wise show
+
+function myMeassage1(){
+    alert("Hello EveryOne");
+}
+
+function myMeassage2(){
+    alert("this is para");
+}
+
+
+//paragraph wise show
+
+function myMeassage3(){
+    var myVar=document.querySelector("#para1");
+
+    myVar.innerHTML="You click on 3 no para";
+}
+
+
+function myMeassage4(){
+    var myVar=document.querySelector("#para2");
+
+    myVar.innerHTML="You click on 4 no para";
+}
+
+
+/*IMG 
+
+var myVar = document.querySelector("#Image");
+function mypicture1(){
+    myVar.src="images/rafi1.jpg";
+}
+
+function mypicture2(){
+    myVar.src="images/rafi2.jpg";
+}
+*/
+
