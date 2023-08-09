e96040ae0bd61612b823dc2ee2b57d9c7d15fbe5
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-09 21:46:48 +0600

    Add files via upload

---
+++ Audio Play/index.js
@@ -0,0 +1,28 @@
+for(var i=0; i<3;i++){
+document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+    var text = this.innerHTML;
+    console.log(text);
+    audioPlay(text);
+    
+})
+}
+
+
+function audioPlay(text){
+    switch(text){
+        case "a":
+            var audio=new Audio("sounds/a.mp3");
+            audio.play();
+            break;
+
+        case "b":
+            var audio=new Audio("sounds/b.mp3");
+            audio.play();
+            break;
+
+        case "c":
+            var audio=new Audio("sounds/c.mp3");
+            audio.play();
+            break;
+    }
+}
