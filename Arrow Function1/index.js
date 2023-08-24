dcc605b4bf3819fabaff38f88d7ba32c1309db64
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-24 20:17:45 +0600

    Add files via upload

---
+++ Arrow Function1/index.js
@@ -0,0 +1,20 @@
+function display1(){console.log("Iam Display1")}
+display1();
+
+//Arrow Function
+const display2=()=>{ console.log("Iam Display2")}
+display2();
+
+//returnType arrowFunction
+const message3 = () => "Hi,Rafi Khan";
+console.log(message3());
+
+
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// console.log(add(10,20));
+
+
+const add = (num1,num2) => num1 + num2;
+console.log(add(10,20));
