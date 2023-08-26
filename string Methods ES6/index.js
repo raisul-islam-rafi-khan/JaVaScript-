7cff8d100eb5276c383c978fdbe49f701e3293a0
Raisul Islam Rafi <126807399+raisul-islam-rafi-khan@users.noreply.github.com>
2023-08-26 22:21:13 +0600

    Add files via upload

---
+++ string Methods ES6/index.js
@@ -0,0 +1,21 @@
+//these every condition return --> (True or False)
+//all these methods are case sensetive
+
+//startsWith(searchString,position)
+const message ="Hi!This is Rafi Khan";
+console.log(message.startsWith("Hi",0)) //if we use small letter then it's gives false
+
+//endsWith(searchString,length)
+const message1 ="Hi!This is Rafi";
+console.log(message1.endsWith("Rafi"))
+
+
+//includes(searchString,position)
+const message2 ="Hi!This is Rafi";
+console.log(message2.endsWith("rafi")) //includes search actually this letter or anything is herer on the ,message or not if yes then true otherwise return false
+
+
+
+
+
+
